3d6de211c81d87f53624e9eff53402acb564d7c6
arielbk <buchwald.kearney@gmail.com>
2020-05-12 22:38:35 +0200

    Pages, routes and layout

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,16 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <main>
+        <p>
+          This is a site that I'm using to check out Redwood js and see how it
+          all works.
+        </p>
+      </main>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
